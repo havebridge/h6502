f0c216d0babaf6e1ac017bf14e03666c5077acf9
havebridge <meyl0r@yandex.com>
2021-10-26 14:47:46 +0300

    LDA, LDX, LDY

--- gtestLoadRegister.cpp
+++ gtestLoadRegister.cpp
@@ -16,14 +16,14 @@ static void CheckStatusFlag(const struct CPU& cpu1, const struct CPU& cpu2)
 	EXPECT_EQ(cpu1.Flags[overflowFlag], cpu2.Flags[overflowFlag]);
 }
 
-TEST(testLoadReg, CPUcyclesNeg)
+TEST(testLoadRegister, CPUcyclesNeg)
 {
 	const uint32_t cycles = 0;
 	uint32_t numCycles = Execute(&gtestLoadcpu, &gtestLoadmem, cycles);
 	EXPECT_EQ(numCycles, 0);
 }
 
-TEST(testLoadReg, LDA_IM_TEST_ZERO_FLAG)
+TEST(testLoadRegister, LDA_IM_TEST_ZERO_FLAG)
 {
 	ResetCpu(&gtestLoadcpu, &gtestLoadmem);
 
@@ -172,52 +172,52 @@ static void gtestLoadRegisterAbsoluteY(struct CPU* cpu, struct memory* mem, byte
 	CheckStatusFlag(*cpu, cpuCopy);
 }
 
-TEST(testLoadReg, LDA_IM_TEST)									// LDA immidiately
+TEST(testLoadRegister, LDA_IM_TEST)									// LDA immidiately
 {
 	gtestLoadRegisterIM(&gtestLoadcpu, &gtestLoadmem, LDA_IM, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDX_IM_TEST)									// LDX immidiately
+TEST(testLoadRegister, LDX_IM_TEST)									// LDX immidiately
 {
 	gtestLoadRegisterIM(&gtestLoadcpu, &gtestLoadmem, LDX_IM, &gtestLoadcpu.x);
 }
 
-TEST(testLoadReg, LDY_IM_TEST)									// LDY immidiately
+TEST(testLoadRegister, LDY_IM_TEST)									// LDY immidiately
 {
 	gtestLoadRegisterIM(&gtestLoadcpu, &gtestLoadmem, LDY_IM, &gtestLoadcpu.y);
 }
 
-TEST(testLoadReg, LDA_ZP_TEST)									// LDA zero page
+TEST(testLoadRegister, LDA_ZP_TEST)									// LDA zero page
 {
 	gtestLoadRegisterZP(&gtestLoadcpu, &gtestLoadmem, LDA_ZP, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDX_ZP_TEST)									// LDX zero page
+TEST(testLoadRegister, LDX_ZP_TEST)									// LDX zero page
 {
 	gtestLoadRegisterZP(&gtestLoadcpu, &gtestLoadmem, LDX_ZP, &gtestLoadcpu.x);
 }
 
-TEST(testLoadReg, LDY_ZP_TEST)									// LDY zero page
+TEST(testLoadRegister, LDY_ZP_TEST)									// LDY zero page
 {
 	gtestLoadRegisterZP(&gtestLoadcpu, &gtestLoadmem, LDY_ZP, &gtestLoadcpu.y);
 }
 
-TEST(testLoadReg, LDA_ZPX_TEST)								// LDA zero page + x register
+TEST(testLoadRegister, LDA_ZPX_TEST)								// LDA zero page + x register
 {
 	gtestLoadRegisterZPX(&gtestLoadcpu, &gtestLoadmem, LDA_ZPX, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDX_ZPY_TEST)								// LDX zero page + y register
+TEST(testLoadRegister, LDX_ZPY_TEST)								// LDX zero page + y register
 {
 	gtestLoadRegisterZPY(&gtestLoadcpu, &gtestLoadmem, LDX_ZPY, &gtestLoadcpu.x);
 }
 
-TEST(testLoadReg, LDY_ZPX_TEST)								// LDY zero page + x register
+TEST(testLoadRegister, LDY_ZPX_TEST)								// LDY zero page + x register
 {
 	gtestLoadRegisterZPX(&gtestLoadcpu, &gtestLoadmem, LDY_ZPX, &gtestLoadcpu.y);
 }
 
-TEST(testLoadReg, LDA_ZPX_WRAP_TEST)
+TEST(testLoadRegister, LDA_ZPX_WRAP_TEST)
 {
 	ResetCpu(&gtestLoadcpu, &gtestLoadmem);
 
@@ -235,44 +235,44 @@ TEST(testLoadReg, LDA_ZPX_WRAP_TEST)
 }
 
 
-TEST(testLoadReg, LDA_ABS_TEST)								// LDA absolute
+TEST(testLoadRegister, LDA_ABS_TEST)								// LDA absolute
 {
 	gtestLoadRegisterAbsolute(&gtestLoadcpu, &gtestLoadmem, LDA_ABS, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDX_ABS_TEST)								// LDX absolute
+TEST(testLoadRegister, LDX_ABS_TEST)								// LDX absolute
 {
 	gtestLoadRegisterAbsolute(&gtestLoadcpu, &gtestLoadmem, LDX_ABS, &gtestLoadcpu.x);
 }
 
-TEST(testLoadReg, LDY_ABS_TEST)								// LDY absolute
+TEST(testLoadRegister, LDY_ABS_TEST)								// LDY absolute
 {
 	gtestLoadRegisterAbsolute(&gtestLoadcpu, &gtestLoadmem, LDY_ABS, &gtestLoadcpu.y);
 }
 
 
-TEST(testLoadReg, LDA_ABSX_TEST)							// LDA absolute + x register
+TEST(testLoadRegister, LDA_ABSX_TEST)							// LDA absolute + x register
 {
 	gtestLoadRegisterAbsoluteX(&gtestLoadcpu, &gtestLoadmem, LDA_ABSX, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDA_ABSY_TEST)							// LDA absolute + y register
+TEST(testLoadRegister, LDA_ABSY_TEST)							// LDA absolute + y register
 {
 	gtestLoadRegisterAbsoluteY(&gtestLoadcpu, &gtestLoadmem, LDA_ABSY, &gtestLoadcpu.acc);
 }
 
-TEST(testLoadReg, LDX_ABSX_TEST)							// LDX absolute + y register
+TEST(testLoadRegister, LDX_ABSX_TEST)							// LDX absolute + y register
 {
 	gtestLoadRegisterAbsoluteY(&gtestLoadcpu, &gtestLoadmem, LDX_ABSY, &gtestLoadcpu.x);
 }
 
-TEST(testLoadReg, LDY_ABSX_TEST)							// LDY absolute + x register
+TEST(testLoadRegister, LDY_ABSX_TEST)							// LDY absolute + x register
 {
 	gtestLoadRegisterAbsoluteX(&gtestLoadcpu, &gtestLoadmem, LDY_ABSX, &gtestLoadcpu.y);
 }
 
 
-TEST(testLoadReg, LDA_INDX_TEST)
+TEST(testLoadRegister, LDA_INDX_TEST)
 {
 	ResetCpu(&gtestLoadcpu, &gtestLoadmem);
 	gtestLoadcpu.x = 0x04;
@@ -293,7 +293,7 @@ TEST(testLoadReg, LDA_INDX_TEST)
 	CheckStatusFlag(gtestLoadcpu, cpuCopy);
 }
 
-TEST(testLoadReg, LDA_INDY_TEST)
+TEST(testLoadRegister, LDA_INDY_TEST)
 {
 	ResetCpu(&gtestLoadcpu, &gtestLoadmem);
 	gtestLoadcpu.y = 0x04;
@@ -314,7 +314,7 @@ TEST(testLoadReg, LDA_INDY_TEST)
 	CheckStatusFlag(gtestLoadcpu, cpuCopy);
 }
 
-TEST(testLoadReg, LDA_INDY_BOUNDARY_TEST)
+TEST(testLoadRegister, LDA_INDY_BOUNDARY_TEST)
 {
 	ResetCpu(&gtestLoadcpu, &gtestLoadmem);
 	gtestLoadcpu.y = 0xFF;
