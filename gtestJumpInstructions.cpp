c87886b62ae381ffd59258638f86dc2c753096fa
havebridge <meyl0r@yandex.com>
2021-10-27 11:25:52 +0300

    JSR, RTS, JMP

--- gtestJumpInstructions.cpp
+++ gtestJumpInstructions.cpp
@@ -13,16 +13,56 @@ TEST(testJumpInstructions, JSR_TEST)
 	gtestJumpInstrmem.Data[0xFF00] = JSR;
 	gtestJumpInstrmem.Data[0xFF01] = 0x00;
 	gtestJumpInstrmem.Data[0xFF02] = 0x80;
-	gtestJumpInstrmem.Data[0x8000] = RTS;
-	gtestJumpInstrmem.Data[0xFF03] = LDA_IM;
-	gtestJumpInstrmem.Data[0xFF04] = 0x42;
+	gtestJumpInstrmem.Data[0x8000] = RTS;;
 
-	const uint32_t ExpectedCycles = 6 + 6 + 2;
+	const uint32_t ExpectedCycles = 6 + 6;
 
 	uint32_t numCycles = Execute(&gtestJumpInstrcpu, &gtestJumpInstrmem, ExpectedCycles);
 	const struct CPU cpuCopy = gtestJumpInstrcpu;
 
 	EXPECT_EQ(numCycles, ExpectedCycles);
-	EXPECT_EQ(gtestJumpInstrcpu.acc, 0x42);
 	EXPECT_EQ(gtestJumpInstrcpu.sp, cpuCopy.sp);
+	EXPECT_EQ(gtestJumpInstrcpu.pc, 0xFF03);
+}
+
+TEST(testJumpInstructions, JUMP_ABS_TEST)
+{
+	ResetCpu(&gtestJumpInstrcpu, &gtestJumpInstrmem);
+	gtestJumpInstrcpu.pc = 0xFF00;
+
+	gtestJumpInstrmem.Data[0xFF00] = JMP_ABS;
+	gtestJumpInstrmem.Data[0xFF01] = 0x00;
+	gtestJumpInstrmem.Data[0xFF02] = 0x80;
+
+
+	const uint32_t ExpectedCycles = 3;
+
+	uint32_t numCycles = Execute(&gtestJumpInstrcpu, &gtestJumpInstrmem, ExpectedCycles);
+	const struct CPU cpuCopy = gtestJumpInstrcpu;
+
+	EXPECT_EQ(numCycles, ExpectedCycles);
+	EXPECT_EQ(gtestJumpInstrcpu.sp, cpuCopy.sp);
+	EXPECT_EQ(gtestJumpInstrcpu.pc, 0x8000);
+}
+
+
+TEST(testJumpInstructions, JUMP_IND_TEST)
+{
+	ResetCpu(&gtestJumpInstrcpu, &gtestJumpInstrmem);
+	gtestJumpInstrcpu.pc = 0xFF00;
+
+	gtestJumpInstrmem.Data[0xFF00] = JMP_IND;
+	gtestJumpInstrmem.Data[0xFF01] = 0x00;
+	gtestJumpInstrmem.Data[0xFF02] = 0x80;
+	gtestJumpInstrmem.Data[0x8000] = 0x00;
+	gtestJumpInstrmem.Data[0x8001] = 0x90;
+
+	const uint32_t ExpectedCycles = 5;
+
+	uint32_t numCycles = Execute(&gtestJumpInstrcpu, &gtestJumpInstrmem, ExpectedCycles);
+	const struct CPU cpuCopy = gtestJumpInstrcpu;
+
+	EXPECT_EQ(numCycles, ExpectedCycles);
+	EXPECT_EQ(gtestJumpInstrcpu.sp, cpuCopy.sp);
+	EXPECT_EQ(gtestJumpInstrcpu.pc, 0x9000);
 }
